c9dfbe30facbe06df713b0ff43372675c2e5c0b0
2pac <tarasyaremabcn@gmail.com>
2025-07-16 11:25:44 +0200

    cubic feedback

--- browser_use/dom/dom_tree/index.js
+++ browser_use/dom/dom_tree/index.js
@@ -1286,7 +1286,7 @@
     };
 
     const extraAttributesTagMap = {
-      'input': ['type', 'value', 'placeholder', 'name', 'checked', 'readonly', 'disabled'],
+      'input': ['type', 'placeholder', 'name', 'checked', 'readonly', 'disabled'],
     }
 
     // Get attributes for interactive elements or potential text containers
